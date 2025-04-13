1e82f34310c2a2725005e495481f068c64b3ae92
Alex <a.amiot99@gmail.com>
2025-04-13 01:13:16 -0400

    Added a few things to Dashboard

--- mes_dashboard.py
+++ mes_dashboard.py
@@ -10,9 +10,27 @@ conn = sqlite3.connect("order_log.db")
 df = pd.read_sql_query("SELECT * FROM order_logs",conn)
 conn.close()
 
+
 run_ids = df["run_id"].unique() #Get all unique run IDs
 selected_run = st.selectbox("Select a RUN_ID", run_ids) #Add dropdown to select one
 filtered_df = df[df["run_id"] == selected_run] #Filter the table based on selection
 
+columns_to_show = [
+    "order_id", "model", "maker", "product_id",
+    "quantity", "due_date", "status", "origin",
+    "created_at", "log_timestamp"
+]
+
+selected_statuses = st.multiselect("Filter by Status", filtered_df["status"].unique())
+
+if selected_statuses:
+    filtered_df = filtered_df[filtered_df["status"].isin(selected_statuses)]
+
+
 st.subheader("All Logged Orders")
-st.dataframe(filtered_df)
+st.dataframe(filtered_df[columns_to_show])
+
+# Show order status counts as a bar chart
+st.subheader("Order Status Breakdown")
+status_counts = filtered_df["status"].value_counts()
+st.bar_chart(status_counts)
